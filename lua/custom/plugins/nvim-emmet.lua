16ba373b0e7371135845cdf75f67bd7ece7d2ee6
meiggan-circa <m.circaa@gmail.com>
2025-02-04 15:01:49 +0800

    created separate rc for biims development

--- lua/custom/plugins/nvim-emmet.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'olrtg/nvim-emmet',
-  config = function()
-    vim.keymap.set({ 'n', 'v' }, '<leader><Tab>', require('nvim-emmet').wrap_with_abbreviation, { desc = '[Emmet]Wrap with Abbrevation' })
-  end,
-}
