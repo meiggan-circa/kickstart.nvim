c40404e9f740f19d154a4c0225fb7aeaf031e689
meiggan-circa <m.circaa@gmail.com>
2025-02-19 14:31:37 +0800

    disabled autopairs plugin

--- init.lua
+++ init.lua
@@ -775,7 +775,7 @@ require('lazy').setup {
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- { import = 'custom.plugins' },
