244c416967e3b925746a6cdb04484f61358a7a0d
meiggan-circa <m.circaa@gmail.com>
2025-09-29 04:20:08 +0800

    config: add and update options, rebind vim motions and add keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -36,4 +36,52 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Rebinded vim motions to center current line in screen when moving
+-- Makes it easy for me to navigate/scroll thru lines of code/files
+vim.keymap.set('n', 'j', 'jzz', { desc = 'Move down one line' })
+vim.keymap.set('n', 'k', 'kzz', { desc = 'Move up one line' })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half a screen' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half a screen' })
+
+vim.keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'Scroll down a full screen' })
+vim.keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'Scroll up a full screen' })
+
+vim.keymap.set('n', 'G', 'Gzz', { desc = 'Go to last line / specified line' })
+
+vim.keymap.set('n', 'H', 'Hzz', { desc = 'Move to top of screen' })
+vim.keymap.set('n', 'L', 'Lzz', { desc = 'Move to bottom of screen' })
+
+vim.keymap.set('n', '{', '{zz', { desc = 'Move up to start of previous paragraph' })
+vim.keymap.set('n', '}', '}zz', { desc = 'Move up to start of next paragraph' })
+
+vim.keymap.set('n', 'n', 'nzz', { desc = 'Forward search last pattern' })
+vim.keymap.set('n', 'N', 'Nzz', { desc = 'Backward search last pattern' })
+
+vim.keymap.set('n', '<C-i>', '<C-i>zz', { desc = 'Go forward in jump list' })
+vim.keymap.set('n', '<C-o>', '<C-o>zz', { desc = 'Go backward in jump list' })
+
+vim.keymap.set('v', 'o', 'ozz', { desc = 'Move the cursor to start/end of selection' })
+vim.keymap.set('v', 'O', 'Ozz', { desc = 'Move the cursor to start/end of blockwise-selection' })
+
+-- Personal keymaps
+vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save file' })
+vim.keymap.set({ 'i', 'v' }, '<C-s>', '<Esc>:w<CR>', { desc = 'Save file' })
+
+vim.keymap.set('n', 'Za', ':wqa<CR>', { desc = 'Quit and save all windows' })
+
+vim.keymap.set('n', '<leader>T', ':tabnew<CR>', { desc = 'Open New Tab' })
+
+vim.keymap.set('n', '<leader>lz', ':Lazy<CR>', { desc = 'Open Lazy Plugin Manager' })
+vim.keymap.set('n', '<leader>ms', ':Mason<CR>', { desc = 'Open Mason Package Manager' })
+
+vim.keymap.set('n', '<leader><leader>s', '<cmd>source %<CR>', { desc = 'Source entire file' })
+vim.keymap.set('n', '<leader>s', ':.lua<CR>', { desc = 'Source current line' })
+vim.keymap.set('v', '<leader>s', ':lua<CR>', { desc = 'Source selected lines' })
+
+vim.keymap.set('n', '<leader>ms', ':Mason<CR>', { desc = 'Open Mason Package Manager' })
+vim.keymap.set('n', '<leader>lz', ':Lazy<CR>', { desc = 'Open Lazy Plugin Manager' })
+
+vim.keymap.set('n', '<leader>z', vim.cmd.UndotreeToggle, { desc = 'Toggle Undotree' })
+
 -- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,7 @@
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -43,7 +43,7 @@ vim.o.timeoutlen = 300
 
 -- Configure how new splits should be opened
 vim.o.splitright = true
-vim.o.splitbelow = true
+vim.o.splitbelow = false
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -69,5 +69,22 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Number of spaces to use for each step of auto indent
+vim.o.shiftwidth = 2
+-- Number of spaces a tab uses in a file
+vim.o.tabstop = 2
+
+-- Width of the column used to indicate folds
+vim.o.foldcolumn = '1'
+-- Sets fold level. Closes folds with a level higher than specified value
+-- 0 = all folds closed, 1 = some folds closed, 99 = no folds closed
+vim.o.foldlevel = 99
+-- 'foldlevel' when starting to edit a file
+vim.o.foldlevelstart = 99
+-- Set to display all folds open
+vim.o.foldenable = true
+-- Sets characters to use for displaying open and closed folds
+vim.o.fillchars = [[foldopen:,foldclose:]]
 
 -- vim: ts=2 sts=2 sw=2 et
