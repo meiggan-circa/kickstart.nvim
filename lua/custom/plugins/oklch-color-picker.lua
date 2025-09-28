7ddb880c6e54a38b7038ebd3c79148c932eb0b83
meiggan-circa <m.circaa@gmail.com>
2025-09-29 05:16:04 +0800

    add: primeagen harpoon, line commenter, color picker plugin

---
+++ lua/custom/plugins/oklch-color-picker.lua
@@ -0,0 +1,18 @@
+return {
+  'eero-lehtinen/oklch-color-picker.nvim',
+  event = 'VeryLazy',
+  version = '*',
+  keys = {
+    -- One handed keymap recommended, you will be using the mouse
+    {
+      '<leader>V',
+      function()
+        require('oklch-color-picker').pick_under_cursor()
+      end,
+      desc = 'Color pick under cursor',
+    },
+  },
+  ---@type oklch.Opts
+  opts = {},
+}
+-- vim: ts=2 sts=2 sw=2 et
