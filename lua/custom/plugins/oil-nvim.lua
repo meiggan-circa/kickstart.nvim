d15e026fab5116c05037c22fe4621ed296758516
meiggan-circa <m.circaa@gmail.com>
2025-09-29 06:03:33 +0800

    plugin(oil-nvim): add toggle file detail view function

--- lua/custom/plugins/oil-nvim.lua
+++ lua/custom/plugins/oil-nvim.lua
@@ -1,9 +1,25 @@
+local detail = false
 return {
   'stevearc/oil.nvim',
   enabled = true,
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = { win_options = { signcolumn = 'yes:2' } },
+  opts = {
+    win_options = { signcolumn = 'yes:2' },
+    keymaps = {
+      ['gd'] = {
+        desc = 'Toggle file detail view',
+        callback = function()
+          detail = not detail
+          if detail then
+            require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+          else
+            require('oil').set_columns { 'icon' }
+          end
+        end,
+      },
+    },
+  },
   -- Optional dependencies
   -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
