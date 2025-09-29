a4b423e193a2006f338278852a0cc1623d5534dc
meiggan-circa <m.circaa@gmail.com>
2025-09-29 13:41:53 +0800

    fix: guess-indent.nvim plugin repo name

--- lua/kickstart/plugins/guess-indent.lua
+++ lua/kickstart/plugins/guess-indent.lua
@@ -1,3 +1,3 @@
-return { 'C427/guess-indent.nvim' } -- Detect tabstop and shiftwidth automatically
+return { 'NMAC427/guess-indent.nvim' } -- Detect tabstop and shiftwidth automatically
 
 -- vim: ts=2 sts=2 sw=2 et
